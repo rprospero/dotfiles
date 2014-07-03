4f99abed2c2cf5f9272fa5e2cf34db902caf4030
= <=>
2014-07-03 14:07:20 +0100

    Fix volume keys on work machine

--- xmonad.hs
+++ xmonad.hs
@@ -49,9 +49,9 @@ myConfig = defaultConfig {
              , ((0, xK_Print), spawn "scrot")
              , ((mod4Mask, xK_p), spawn "$(~/.cabal/bin/yeganesh -x -- -b -nb black)")
              , ((mod4Mask .|. shiftMask, xK_Return), spawn "xterm")
-             , ((0, xK_AudioLowerVolume   ), spawn "amixer set Master 2%-")
-             , ((0, xK_AudioRaiseVolume   ), spawn "amixer set Master 2%+")
-             , ((0, xK_AudioMute          ), spawn "amixer set Master toggle")
+             , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2%-")
+             , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2%+")
+             , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle")
              ]
 
 
