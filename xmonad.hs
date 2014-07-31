d295ae606eca1fe8e6dea60dec01dd39a5789767
= <=>
2014-07-31 10:21:11 +0100

    Reinstated yeganesh

--- xmonad.hs
+++ xmonad.hs
@@ -47,7 +47,7 @@ myConfig = defaultConfig {
              [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
              , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
              , ((0, xK_Print), spawn "scrot")
-             , ((mod4Mask, xK_p), spawn "dmenu_run -b -nb black -nf white")
+             , ((mod4Mask, xK_p), spawn "$(~/.cabal/bin/yeganesh -x -- -b -nb black)")
              , ((mod4Mask, xK_e), spawn "emacsclient -c")
              , ((mod4Mask .|. shiftMask, xK_Return), spawn "xterm")
              , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2%-")
