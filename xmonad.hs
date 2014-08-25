94d59089ae02e26f34ca28c98cefed9e02f250fd
Adam Washington <rprospero@gmail.com>
2014-08-25 22:51:40 +0100

    Add mutt to e-mail workspace

--- xmonad.hs
+++ xmonad.hs
@@ -22,14 +22,14 @@ myManageHook = composeAll . concat $
     viewShift = doF . liftM2 (.) W.greedyView W.shift
     myClassWebShifts = ["Firefox","Opera"]
     myClassEmacsShifts = ["Emacs"]
-    myClassChatShifts = ["Pidgin","Thunderbird","Geary"]
+    myClassChatShifts = ["Pidgin","Thunderbird","Geary","mutt"]
     myClassDocumentsShifts = ["Evince"]
 
 main = do
   xmproc <- spawn "conky -c ~/.xmonad/.conkyrc | dzen2 -fg cyan -fn \"inconsolata:pixelsize=12\" -w 832 -l 2 -y -1 -bg black"
   xmonad myConfig
 
-  
+
 --myStatusBar = statusBar "conky -c ~/.xmonad/.conkyrc | dzen2 -fg cyan -fn \"inconsolata:pixelsize=12\" -w 1100 -l 2 -y -1" myPP toggleStrutsKey
 
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
