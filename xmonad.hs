fa734ff4661364a35962ab89fd3419c1e215c3c3
Adam Washington <a.washington@sheffield.ac.uk>
2015-01-06 15:26:01 +0000

    Add thunar to xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -49,6 +49,7 @@ myConfig = defaultConfig {
              , ((0, xK_Print), spawn "scrot")
              , ((mod4Mask, xK_p), spawn "$(~/.cabal/bin/yeganesh -x -- -b -nb black)")
              , ((mod4Mask, xK_e), spawn "emacsclient -c")
+             , ((mod4Mask, xK_t), spawn "thunar")
              , ((mod4Mask .|. shiftMask, xK_Return), spawn "xterm")
              , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2%-")
              , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2%+")
