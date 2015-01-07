3306243eaf3d2b654d14f1a70c945bd0b00235f7
Adam Washington <a.washington@sheffield.ac.uk>
2015-01-07 13:30:29 +0000

    Added screensaver

--- xmonad.hs
+++ xmonad.hs
@@ -41,7 +41,7 @@ myConfig = defaultConfig {
                startupHook = setWMName "LG3D"
              }
              `additionalKeys`
-             [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
+             [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -l")
              , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
              , ((0, xK_Print), spawn "scrot")
              , ((mod4Mask, xK_p), spawn "$(~/.cabal/bin/yeganesh -x -- -b -nb black)")
