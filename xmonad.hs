db997d5a25c611a8c0bfed25746275032b2c5a8d
Adam Washington <a.washington@sheffield.ac.uk>
2015-03-19 16:20:49 +0000

    Fix emacs hotkey for multiple screens

--- xmonad.hs
+++ xmonad.hs
@@ -57,7 +57,7 @@ myConfig = defaultConfig {
              , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
              , ((0, xK_Print), spawn "scrot")
              , ((mod4Mask, xK_p), spawn "$(~/.cabal/bin/yeganesh -x -- -b -nb black)")
-             , ((mod4Mask, xK_e), spawn "emacsclient -c")
+             , ((mod4Mask .|. mod1Mask, xK_e), spawn "emacsclient -c")
              , ((mod4Mask, xK_t), spawn "thunar")
              , ((mod4Mask .|. shiftMask, xK_Return), spawn "xterm")
              , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2%-")
