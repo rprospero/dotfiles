da7d7284e13c82c3086856d3dd9744675fa7c6af
Adam Washington <a.washington@sheffield.ac.uk>
2016-11-02 10:50:31 +0000

    Move some auctex out of custom

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,15 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-PDF-mode t t)
- '(TeX-view-program-list (quote (("Okular" "okular --unique %o#src:%n%b"))))
- '(TeX-view-program-selection
-   (quote
-    (((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "Okular")
-     (output-pdf "open")
-     (output-html "xdg-open"))))
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-term-color-vector
