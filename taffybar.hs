2f4c2bc9fe708e76ce48446f7974dcb1271463f7
Adam Washington <rprospero@gmail.com>
2017-02-01 15:58:30 +0000

    Reintroduce weather

--- taffybar.hs
+++ taffybar.hs
@@ -1,8 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Applicative (empty)
 import Control.Monad.Trans (liftIO)
+import Data.Aeson
 import Data.Char (toLower)
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (fromStrict)
 import Data.Foldable (foldl')
 import Data.List (isSuffixOf, isPrefixOf, isInfixOf)
 import Data.Monoid ((<>))
+import Data.Text (unpack)
+import Network.Download (openURI)
 import Network.HostName
 import System.Process
 import Text.Printf
@@ -201,6 +209,101 @@ appleIcon = faicon "F179"
 androidIcon = faicon "F17B"
 amazonIcon = faicon "F270"
 
+rawWeatherIcon :: Int -> Bool -> String
+rawWeatherIcon 200 = thunderstormIcon
+rawWeatherIcon 201 = thunderstormIcon
+rawWeatherIcon 202 = thunderstormIcon
+rawWeatherIcon 210 = thunderstormIcon
+rawWeatherIcon 211 = thunderstormIcon
+rawWeatherIcon 212 = thunderstormIcon
+rawWeatherIcon 221 = thunderstormIcon
+rawWeatherIcon 230 = thunderstormIcon
+rawWeatherIcon 231 = thunderstormIcon
+rawWeatherIcon 232 = thunderstormIcon
+rawWeatherIcon 300 = drizzleIcon
+rawWeatherIcon 301 = drizzleIcon
+rawWeatherIcon 302 = drizzleIcon
+rawWeatherIcon 310 = drizzleIcon
+rawWeatherIcon 311 = drizzleIcon
+rawWeatherIcon 312 = drizzleIcon
+rawWeatherIcon 313 = drizzleIcon
+rawWeatherIcon 314 = drizzleIcon
+rawWeatherIcon 321 = drizzleIcon
+rawWeatherIcon 500 = rainIcon
+rawWeatherIcon 501 = rainIcon
+rawWeatherIcon 502 = rainIcon
+rawWeatherIcon 503 = rainIcon
+rawWeatherIcon 504 = rainIcon
+rawWeatherIcon 511 = sleetIcon
+rawWeatherIcon 520 = showerIcon
+rawWeatherIcon 521 = showerIcon
+rawWeatherIcon 522 = showerIcon
+rawWeatherIcon 531 = showerIcon
+rawWeatherIcon 600 = snowIcon
+rawWeatherIcon 601 = snowIcon
+rawWeatherIcon 602 = snowIcon
+rawWeatherIcon 611 = snowIcon
+rawWeatherIcon 612 = snowIcon
+rawWeatherIcon 615 = snowIcon
+rawWeatherIcon 616 = snowIcon
+rawWeatherIcon 620 = snowIcon
+rawWeatherIcon 621 = snowIcon
+rawWeatherIcon 622 = snowIcon
+rawWeatherIcon 701 = mistIcon
+rawWeatherIcon 711 = mistIcon
+rawWeatherIcon 721 = mistIcon
+rawWeatherIcon 731 = mistIcon
+rawWeatherIcon 741 = mistIcon
+rawWeatherIcon 751 = mistIcon
+rawWeatherIcon 761 = mistIcon
+rawWeatherIcon 762 = mistIcon
+rawWeatherIcon 771 = mistIcon
+rawWeatherIcon 781 = mistIcon
+rawWeatherIcon 800 = clearIcon
+rawWeatherIcon 801 = cloudyIcon
+rawWeatherIcon 802 = cloudyIcon
+rawWeatherIcon 803 = cloudyIcon
+rawWeatherIcon 804 = cloudyIcon
+rawWeatherIcon 900 =  const "?"
+rawWeatherIcon 901 =  const "?"
+rawWeatherIcon 902 =  const "?"
+rawWeatherIcon 903 =  const "?"
+rawWeatherIcon 904 =  const "?"
+rawWeatherIcon 905 =  const "?"
+rawWeatherIcon 906 =  const "?"
+rawWeatherIcon 951 =  const "?"
+rawWeatherIcon 952 =  const "?"
+rawWeatherIcon 953 =  const "?"
+rawWeatherIcon 954 =  const "?"
+rawWeatherIcon 955 =  const "?"
+rawWeatherIcon 956 =  const "?"
+rawWeatherIcon 957 =  const "?"
+rawWeatherIcon 958 =  const "?"
+rawWeatherIcon 959 =  const "?"
+rawWeatherIcon 960 =  const "?"
+rawWeatherIcon 961 =  const "?"
+rawWeatherIcon 962 =  const "?"
+rawWeatherIcon _ = const "?"
+
+clearIcon True = wicon "F00D"
+clearIcon False = wicon "F02E"
+cloudyIcon True = wicon "f002"
+cloudyIcon False = wicon "f031"
+overcastIcon _ = wicon "f013"
+thunderstormIcon True = wicon "f010"
+thunderstormIcon False = wicon "f03b"
+drizzleIcon True = wicon "f00b"
+drizzleIcon False = wicon "f039"
+rainIcon True = wicon "f008"
+rainIcon False = wicon "f036"
+showerIcon True = wicon "f009"
+showerIcon False = wicon "f037"
+snowIcon True = wicon "f00a"
+snowIcon False = wicon "f038"
+sleetIcon True = wicon "f0b2"
+sleetIcon False = wicon "f0b4"
+mistIcon _ = wicon "f014"
+
 ---------------  Battery Icon Code
 
 batteryFullIcon = faicon "F240"
@@ -258,6 +361,101 @@ appropriateBattery x
   | batteryPercentage x < 60.0 = colorize "#b58900" "" $ batteryHalfIcon
   | batteryPercentage x < 80.0 = batteryThreeQuarterIcon
   | otherwise = batteryFullIcon
+
+-- Weather Applet
+
+data Weather = Weather {
+  weatherCoord :: Coord,
+  weatherStates :: [WeatherState],
+  weatherBase :: String,
+  weatherMain :: WeatherStats,
+  weatherDt :: Int,
+  -- weatherSys :: WeatherSys
+  weatherId :: Int,
+  weatherName :: String
+  }
+  deriving (Show, Eq)
+
+instance FromJSON Weather where
+  parseJSON (Object v) = Weather <$>
+    v .: "coord" <*>
+    v .: "weather" <*>
+    v .: "base" <*>
+    v .: "main" <*>
+    v .: "dt" <*>
+    v .: "id" <*>
+    v .: "name"
+
+data WeatherStats = WeatherStats {
+  wsTemp :: Double,
+  wsTempMin :: Double,
+  wsTempMax :: Double,
+  wsPressure :: Int,
+  wsHumidity :: Int
+  }
+  deriving (Show, Eq)
+
+instance FromJSON WeatherStats where
+  parseJSON (Object v) = WeatherStats <$>
+    v .: "temp" <*>
+    v .: "temp_min" <*>
+    v .: "temp_max" <*>
+    v .: "pressure" <*>
+    v .: "humidity"
+
+data WeatherState = WeatherState {
+  wsId :: Int,
+  wsMain :: String,
+  wsDescription :: String,
+  wsIcon :: String
+  }
+  deriving (Show, Eq)
+
+instance FromJSON WeatherState where
+  parseJSON (Object v) = WeatherState <$>
+    v .: "id" <*>
+    v .: "main" <*>
+    v .: "description" <*>
+    v .: "icon"
+
+data Coord = Coord {
+  coordLat :: Double,
+  coordLong :: Double
+  }
+  deriving (Show, Eq)
+
+instance FromJSON Coord where
+  parseJSON (Object v) = Coord <$>
+    v .: "lat" <*>
+    v .: "lon"
+  parseJSON _ = empty
+
+getWeather :: ByteString -> Maybe Weather
+getWeather = decode . fromStrict
+
+localWeather :: String -> IO (Either String Weather)
+localWeather city = do
+  text <- openURI $ "http://api.openweathermap.org/data/2.5/weather?q=" <> city <> "&appid=932841262ade6126df76f1196989509e"
+  return $ case text of
+    Left error -> Left error
+    Right body -> case getWeather body of
+      Nothing -> Left $ "Cannot parse weather: " <> take 20 (show text)
+      Just w -> Right w
+
+weatherIcon :: Weather -> String
+weatherIcon w =
+  case weatherStates w of
+    [] -> "No Weather"
+    x:_ ->
+      let daytime = (== 'd') . head . reverse . wsIcon $ x
+      in flip rawWeatherIcon daytime $ wsId x
+
+weather :: String -> IO String
+weather city = do
+  wea <- localWeather city
+  case wea of
+    Left err -> return err
+    Right w -> return $ weatherIcon w
 
 --  Widget Utilities
 
@@ -382,7 +580,7 @@ main = do
         visibleWorkspace = colorize "#2aa198" "" . workspaceMangler,
         widgetSep = " | "}
       note = notifyAreaNew defaultNotificationConfig
-      -- wea = weatherNew (defaultWeatherConfig "EGVN"){ weatherTemplate = "$tempC$ C @ $humidity$" } 10
+      wea = pollingLabelNew "waiting" 10 (weather "Didcot") >>= showAndReturn
       mem = myPollingBar 5 memCallback
       net = myPollingBar 1 $ netCallback netref 0
       netup = myPollingBar 1 $ netCallback netref 1
@@ -395,7 +593,7 @@ main = do
                                         , barHeight = 20
                                         , barPosition = Bottom
                                         , endWidgets = [ tray,
-                                                         batteryWidget 5.0, -- wifi,
+                                                         batteryWidget 5.0, wea,
                                                          clock, staticLabel calendarIcon,
                                                          mem, staticLabel verilogIcon] ++
                                                        cpuCharts (cpuCount host) ++
