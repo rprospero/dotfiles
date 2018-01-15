0d313bc7cfc4c005a28e88c42fd150d8f9ec1e75
Adam Washington <adam.washington@stfc.ac.uk>
2018-01-15 09:30:34 +0000

    Allow unfree in configuration

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -39,6 +39,8 @@ in
   # Set your time zone.
   time.timeZone = "Europe/London";
 
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
