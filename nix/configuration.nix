dd96d92dbd5122f7208fe866d3b63dcb03c2c915
Adam Washington <adam.washington@stfc.ac.uk>
2018-01-25 12:48:22 +0000

    Include the full texlive distribution

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -117,7 +117,7 @@ in
         ignoreCollisions = true;
       })
       super-user-spark
-      texlive.combined.scheme-small
+      texlive.combined.scheme-full
       tmux
       unzip
       xfce.thunar
