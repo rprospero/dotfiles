1008436ea275162f3253f9f9e626c067039fc8bc
Adam Washington <rprospero@gmail.com>
2018-04-15 15:41:07 +0100

    Add stylus to colour setup

--- base16/builder.sh
+++ base16/builder.sh
@@ -6,3 +6,4 @@ base16-builder -s ./$1.yaml -t ./gtk-2.0.template -b dark > gtk-2.0
 base16-builder -s ./$1.yaml -t ./gtk-3.0.template -b dark > gtk-3.0
 base16-builder -s ./$1.yaml -t ./emacs.template -b dark > emacs
 base16-builder -s ./$1.yaml -t ./setroot.sh.template -b dark > setroot.sh
+base16-builder -s ./$1.yaml -t ./stylus.css.template -b dark > stylus.css
